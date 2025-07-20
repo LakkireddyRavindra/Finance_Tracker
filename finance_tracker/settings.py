a39dbdd98e7fbd1c1fa17ab50d06c6b09b58d9d1
LakkireddyRavindra <lakkireddyravindra229@gmail.com>
2025-07-20 15:42:09 +0530

    Update ALLOWED_HOSTS with render domain

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['finance-tracker-9oas.onrender.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 INSTALLED_APPS = [
